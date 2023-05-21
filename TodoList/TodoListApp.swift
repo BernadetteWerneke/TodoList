7e2a5aa1bcf9d5114bb1074ca76539f5ebc2ec50
Bernadette Werneke <bernadettewerneke@gmail.com>
2023-05-21 19:07:13 +0200

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Bernadette Werneke on 21.05.23.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
